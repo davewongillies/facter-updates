a7c01e8070a5dcffc02e89a32ab92c3754cb56e6
David Gillies <daveg@dorja.com>
2013-05-28 22:54:35 +1000

    minor adjustments:
    * don't set fact if updates-available doesn't exist
    * default value of 0 for facts

--- lib/facter/updates.rb
+++ lib/facter/updates.rb
@@ -1,33 +1,33 @@
 Facter.add(:updates) do
   setcode do
-    updates = []
     if Facter.value(:lsbdistid) == "Ubuntu"
       if File.exists?('/var/lib/update-notifier/updates-available')
+        updates = 0
         update_file = File.open('/var/lib/update-notifier/updates-available')
         update_file.each_line do |line|
-          line.split("\n").grep(/packages can be updated/) do |m|
-            updates = m.split(" ")[0]
+          if line.split("\n").grep(/packages can be updated/)
+            updates = line.split(" ")[0]
           end
         end
+        updates
       end
     end
-    updates
   end
 end
 
 Facter.add(:updates_security) do
   setcode do
-    updates_security = []
     if Facter.value(:lsbdistid) == "Ubuntu"
       if File.exists?('/var/lib/update-notifier/updates-available')
+        updates_security = 0
         update_file = File.open('/var/lib/update-notifier/updates-available')
         update_file.each_line do |line|
-          line.split("\n").grep(/updates are security updates/) do |m|
-            updates_security = m.split(" ")[0]
+          if line.split("\n").grep(/updates are security updates/)
+            updates_security = line.split(" ")[0]
           end
         end
+        updates_security
       end
     end
-    updates_security
   end
 end
