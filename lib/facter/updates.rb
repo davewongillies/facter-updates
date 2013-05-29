85913b083ef9c32968e5c035c2cc57741445ecb5
David Gillies <dgillies@brandscreen.com>
2013-05-29 11:34:50 +1000

    match lines instead of grep

--- lib/facter/updates.rb
+++ lib/facter/updates.rb
@@ -5,8 +5,8 @@ Facter.add(:updates) do
         updates = 0
         update_file = File.open('/var/lib/update-notifier/updates-available')
         update_file.each_line do |line|
-          if line.split("\n").grep(/packages can be updated/)
-            updates = line.split(" ")[0]
+          if line.match(/packages? can be updated/)
+            updates = line.split(' ')[0]
           end
         end
         updates
@@ -22,8 +22,8 @@ Facter.add(:updates_security) do
         updates_security = 0
         update_file = File.open('/var/lib/update-notifier/updates-available')
         update_file.each_line do |line|
-          if line.split("\n").grep(/updates are security updates/)
-            updates_security = line.split(" ")[0]
+          if line.match(/security updates/)
+            updates_security = line.split(' ')[0]
           end
         end
         updates_security
