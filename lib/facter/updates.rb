6f8ef5cdbf52e635f5ae76a6d9fab8ed75744e68
David Gillies <dgillies@brandscreen.com>
2013-05-30 11:52:55 +1000

    refactor

--- lib/facter/updates.rb
+++ lib/facter/updates.rb
@@ -1,15 +1,15 @@
 Facter.add(:updates) do
   setcode do
-    if Facter.value(:lsbdistid) == "Ubuntu"
+    if Facter.value(:osfamily) == "Debian"
       updates = 0
       if File.exists?('/var/lib/update-notifier/updates-available')
-        update_file = File.open('/var/lib/update-notifier/updates-available')
-        update_file.each_line do |line|
-          if line.match(/packages? can be updated/)
-            updates = line.split(' ')[0]
-          else
-            updates = Facter::Util::Resolution.exec('/usr/bin/apt-get -s -o Debug::NoLocking=true dist-upgrade | grep -c ^Inst')
+        output = File.open('/var/lib/update-notifier/updates-available')
+        if not output.nil?
+          output.each_line do |line|
+            updates = line.split(' ')[0] if line =~ /packages? can be updated/
           end
+        else
+          updates = Facter::Util::Resolution.exec('/usr/bin/apt-get -s -o Debug::NoLocking=true dist-upgrade | grep -c ^Inst')
         end
       else
         updates = Facter::Util::Resolution.exec('/usr/bin/apt-get -s -o Debug::NoLocking=true upgrade | grep -c ^Inst')
@@ -19,13 +19,12 @@ Facter.add(:updates) do
       updates = Facter::Util::Resolution.exec('/usr/sbin/softwareupdate -l|grep -c "\t"')
     elsif Facter.value(:osfamily) == "windows"
       updates = 0
-      get_updates = IO.popen("wmic qfe get", "r")
-      while line = get_updates.gets
-        if line.match(/Update/)
-          updates = updates + 1
+      output = Facter::Util::Resolution.exec('wmic qfe get')
+      if not output.nil?
+        output.each_line do |line|
+          updates = updates + 1 if line =~ /Update/
         end
       end
-      get_updates.close
       updates
     end
   end
@@ -33,26 +32,25 @@ end
 
 Facter.add(:updates_security) do
   setcode do
-    if Facter.value(:lsbdistid) == "Ubuntu"
+    if Facter.value(:osfamily) == "Debian"
       if File.exists?('/var/lib/update-notifier/updates-available')
         updates_security = 0
-        update_file = File.open('/var/lib/update-notifier/updates-available')
-        update_file.each_line do |line|
-          if line.match(/security updates/)
-            updates_security = line.split(' ')[0]
+        output = File.open('/var/lib/update-notifier/updates-available')
+        if not output.nil?
+          output.each_line do |line|
+            updates_security = line.split(' ')[0] if line =~ /security updates/
           end
         end
         updates_security
       end
     elsif Facter.value(:osfamily) == "windows"
       updates_security = 0
-      get_updates = IO.popen("wmic qfe get", "r")
-      while line = get_updates.gets
-        if line.match(/Security Update/)
-          updates_security = updates_security + 1
+      output = Facter::Util::Resolution.exec('wmic qfe get')
+      if not output.nil?
+        output.each_line do |line|
+          updates_security = updates_security + 1 if line =~ /Security Update/
         end
       end
-      get_updates.close
       updates_security
     end
   end
